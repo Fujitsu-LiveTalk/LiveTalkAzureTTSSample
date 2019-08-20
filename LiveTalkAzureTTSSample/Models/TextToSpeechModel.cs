01da5650e39a2263a87035d8601f8f68265b5fa8
FUJITSU Software LiveTalk <47411194+FujitsuSSL-LiveTalk@users.noreply.github.com>
2019-08-20 15:11:13 +0900

    Add files via upload

---
+++ LiveTalkAzureTTSSample/Models/TextToSpeechModel.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Copyright 2019 FUJITSU SOCIAL SCIENCE LABORATORY LIMITED
+ * クラス名　：TextToSpeechModel
+ * 概要      ：Azure Cognitive Services Speech Servicesと連携
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveTalkAzureTTSSample.Models
+{
+    internal class TextToSpeechModel
+    {
+        private string APIKey = "<<<<YOUR_SUBSCRIPTION_KEY>>>>";
+        private string TokenUrl = "https://eastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+        private string Url = "https://eastasia.tts.speech.microsoft.com/cognitiveservices/v1";
+        private string ProxyServer = "";    // PROXY経由なら proxy.hogehoge.jp:8080 のように指定
+        private string ProxyId = "";        // 認証PROXYならIDを指定
+        private string ProxyPassword = "";  // 認証PROXYならパスワードを指定
+        private string AccessToken = string.Empty;
+
+        /// <summary>
+        /// アクセストークンを取得する
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetToken()
+        {
+            this.AccessToken = await FetchTokenAsync().ConfigureAwait(false);
+            Console.WriteLine("Successfully obtained an access token. \n");
+        }
+
+        public async Task<(byte[], string)> TextToSpeechAsync(string text)
+        {
+            try
+            {
+                // パラメタ設定
+                var body =
+                    @"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'>" +
+                    @"<voice name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>" +
+                    text +
+                    "</voice></speak>";
+
+                // プロキシ設定
+                var ch = new HttpClientHandler() { UseCookies = true };
+                if (!string.IsNullOrEmpty(this.ProxyServer))
+                {
+                    var proxy = new System.Net.WebProxy(this.ProxyServer);
+                    if (!string.IsNullOrEmpty(this.ProxyId) && !string.IsNullOrEmpty(this.ProxyPassword))
+                    {
+                        proxy.Credentials = new System.Net.NetworkCredential(this.ProxyId, this.ProxyPassword);
+                    }
+                    ch.Proxy = proxy;
+                }
+                else
+                {
+                    ch.Proxy = null;
+                }
+
+                // Web API呼び出し
+                using (var client = new HttpClient(ch))
+                {
+                    using (var request = new HttpRequestMessage())
+                    {
+                        request.Method = HttpMethod.Post;
+                        request.RequestUri = new Uri(this.Url);
+                        request.Content = new StringContent(body, Encoding.UTF8, "application/ssml+xml");
+                        request.Headers.Add("Authorization", "Bearer " + this.AccessToken);
+                        request.Headers.Add("Connection", "Keep-Alive");
+                        request.Headers.Add("User-Agent", "LiveTalk");
+                        request.Headers.Add("X-Microsoft-OutputFormat", "riff-24khz-16bit-mono-pcm");
+
+
+                        using (var response = await client.SendAsync(request).ConfigureAwait(false))
+                        {
+                            if (response.StatusCode == HttpStatusCode.OK)
+                            {
+                                return (await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false), string.Empty);
+                            }
+                            else
+                            {
+                                return (null, response.StatusCode.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return (null, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// アクセストークンを取得
+        /// </summary>
+        /// <returns></returns>
+        private async Task<string> FetchTokenAsync()
+        {
+            // プロキシ設定
+            var ch = new HttpClientHandler() { UseCookies = true };
+            if (!string.IsNullOrEmpty(this.ProxyServer))
+            {
+                var proxy = new System.Net.WebProxy(this.ProxyServer);
+                if (!string.IsNullOrEmpty(this.ProxyId) && !string.IsNullOrEmpty(this.ProxyPassword))
+                {
+                    proxy.Credentials = new System.Net.NetworkCredential(this.ProxyId, this.ProxyPassword);
+                }
+                ch.Proxy = proxy;
+            }
+            else
+            {
+                ch.Proxy = null;
+            }
+
+            // 認証呼び出し
+            using (var client = new HttpClient(ch))
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", this.APIKey);
+                UriBuilder uriBuilder = new UriBuilder(this.TokenUrl);
+
+                var result = await client.PostAsync(uriBuilder.Uri.AbsoluteUri, null).ConfigureAwait(false);
+                return await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ LiveTalkAzureTTSSample/Program.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Copyright 2019 FUJITSU SOCIAL SCIENCE LABORATORY LIMITED
+ * システム名：LiveTalkAzureTTSSample
+ * 概要      ：LiveTalk-Speech Services連携サンプルアプリ
+*/
+using NAudio.Wave;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiveTalkAzureTTSSample
+{
+    class Program
+    {
+        static FileCollaboration FileInterface;
+        static BlockingCollection<byte[]> AudioQueue = new BlockingCollection<byte[]>();
+        static CancellationTokenSource TokenSource = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            var model = new Models.TextToSpeechModel();
+            var param = new string[]
+            {
+                System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "LiveTalkOutput.csv"),
+            };
+            if (args.Length >= 1)
+            {
+                param[0] = args[0];
+            }
+            Console.WriteLine("InputCSVFileName  :" + param[0]);
+            FileInterface = new FileCollaboration(param[0], "");
+
+            // ファイル入力(LiveTalk常時ファイル出力からの入力)
+            FileInterface.RemoteMessageReceived += async (s) =>
+            {
+                var reg = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                var items = reg.Split(s);
+
+                Console.WriteLine(">>>>>>>");
+                Console.WriteLine("DateTime:" + items[0]);
+                Console.WriteLine("Speaker:" + items[1]);
+                Console.WriteLine("Speech contents:" + items[2]);
+                Console.WriteLine("Translate content:" + items[3]);
+
+                (byte[] waveData, string errorMessage) = await model.TextToSpeechAsync(items[3] == "\"\"" ? items[2] : items[3]);
+                if (waveData != null)
+                {
+                    // 音声合成キューにエントリ
+                    AudioQueue.Add(waveData);
+                }
+                else
+                {
+                    // エラーメッセージ表示
+                    Console.WriteLine(errorMessage);
+                }
+            };
+
+            // 事前に認証トークンを取得（別途、トークン有効期間切れ対応が必要）
+            Task.Factory.StartNew(async () =>
+            {
+                await model.GetToken();
+            });
+
+            // 音声合成キュー処理
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    // 音声合成の再生
+                    if (AudioQueue.TryTake(out byte[] data, -1, TokenSource.Token))
+                    {
+                        using (var ms = new MemoryStream(data))
+                        {
+                            using (var audio = new WaveFileReader(ms))
+                            {
+                                using (var outputDevice = new WaveOutEvent())
+                                {
+                                    outputDevice.Init(audio);
+                                    outputDevice.Play();
+                                    while (outputDevice.PlaybackState == PlaybackState.Playing)
+                                    {
+                                        Thread.Sleep(1000);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            // ファイル監視開始
+            if (System.IO.File.Exists(param[0]))
+            {
+                System.IO.File.Delete(param[0]);
+            }
+            FileInterface.WatchFileSart();
+
+            // 処理終了待ち
+            var message = Console.ReadLine();
+
+            // ファイル監視終了
+            TokenSource.Cancel(true);
+            FileInterface.WatchFileStop();
+        }
+    }
+}
